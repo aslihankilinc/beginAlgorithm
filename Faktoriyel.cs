3f614d9456fed5e8e3fd1ea063668da66f6d2ecc
slhnklnc <aslhanklnc@gmail.com>
2022-01-13 20:26:37 +0300

    Add project files.

---
+++ Faktoriyel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beginAlgorithm
+{
+
+    class Faktoriyel
+    {
+        public static void extraLongFactorials(int n)
+        {
+            BigInteger result = new BigInteger(1);
+           // long result = 1;
+           
+                for (long i = 1; i<n; i++)
+                {
+                result *= i;
+                Console.WriteLine(result.ToString()+"  "+i.ToString());
+            }
+            string a = result.ToString();
+                Console.WriteLine(result.ToString());
+         
+            
+
+        }
+
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            extraLongFactorials(45);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sihirliKare.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beginAlgorithm
+{
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Serialization;
+    using System.Text;
+    using System;
+
+    class Result
+    {
+
+        // static int[,] array = new int[3, 3] { { 4, 8, 2 }, { 4, 5, 7 }, { 6, 1, 6 } };
+        static int[,] array = new int[3, 3] { { 5, 3, 4 }, { 1, 5, 8 }, { 6, 4, 2 } };
+        static int result=0;
+       
+
+        public static void formingMagicSquare(List<List<int>> s)
+        {
+            int[][] array = s.Select(c => c.ToArray()).ToArray();
+            int a = 0;
+            int t = (int)(3 * (Math.Pow(3, 2) + 1)) / 2;
+            Random rand = new Random();
+        topla:
+            int tX = 0;
+            int tY = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                tX += array[a][i];
+                tY += array[i][a];
+            }
+            if (tX != tY || (tX != t || tY != t))
+            {
+                if (tX != t)
+                {
+                    int f = t - tX;
+                    short index = (short)rand.Next(1, 3);
+                    array[a][index] = array[a][index] + f;
+                }
+                if (tY != t)
+                {
+                    int f = t - tY;
+                    short index = (short)rand.Next(1, 3);
+                    array[index][a] = array[index][a] + f;
+                }
+                goto topla;
+            }
+            else if (a < 2)
+            {
+                a++;
+                result++;
+                goto topla;
+            }
+        }
+
+       
+        public static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            int a = 0;
+            int t = (int)(3 *(Math.Pow(3,2) + 1)) / 2;
+            Random rand = new Random();
+        topla:
+            int tX = 0;
+            int tY = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                tX += array[a, i];
+                tY += array[i,a];
+            }
+            if(tX!=tY || (tX != t || tY != t))
+            {
+                if (tX !=t)
+                {
+                    int f = t - tX;
+                    short index = (short)rand.Next(1, 3);
+                    array[a, index] = array[a, index] + f;
+                }
+                if (tY != t)
+                {
+                    int f = t - tY;
+                    short index = (short)rand.Next(1, 3);
+                    array[index,a] = array[index,a] + f;
+                }
+                goto topla;
+            }
+            else if(a<2)
+            {
+                a++;
+                result++;
+                goto topla;
+            }   
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
